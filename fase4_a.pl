1bb6cf6585380b57b4bb1b3b18fbfada3bb7ecab
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2012-12-11 14:47:07 +0100

    add fase 4 parts a and b

---
+++ fase4_a.pl
@@ -0,0 +1,34 @@
+%
+
+eval_fib(N, F) :-
+    eval_fib(N, F, []).
+
+eval_fib(N, F, Conts) :-
+    N > 1,
+    N1 is N - 1,
+    eval_fib(N1, F1, [N2 is N1 - 1, fib(N2, F2), F is F1 + F2|Conts]).
+
+eval_fib(0, 1, Conts) :-
+    eval_true(Conts).
+
+eval_fib(1, 1, Conts) :-
+    eval_true(Conts).
+
+eval_true(Conts) :-
+    continue(Conts).
+
+eval_is(X, Y, Conts) :-
+    X is Y,
+    continue(Conts).
+
+clause_fib(N, F, (N > 1, N1 is N - 1, fib(N1, F1), N2 is N1 - 1, fib(N2, F2), F is F1 + F2)).
+clause_fib(0, 1, true).
+clause_fib(1, 1, true).
+
+continue([]).
+continue([(X is Y)|Conts]) :-
+    eval_is(X, Y, Conts).
+
+continue([fib(N, F)|Conts]) :-
+    eval_fib(N, F, Conts).
+
---
+++ fase4_b.pl
@@ -0,0 +1,56 @@
+%
+
+main :-
+    loop(work).
+
+loop(G) :-
+    call(G),
+    loop(G).
+
+work :-
+    read(Number),
+    work_(Number).
+
+work_(0) :-
+    writeln('I’ve seen enough'),
+    end.
+
+work_(N) :-
+    N > 0,
+    writeln('That’s a nice number.').
+
+%
+
+eval_main :-
+    eval_loop_work([]).
+
+eval_loop_work(Conts) :-
+    eval_read(Number, [work_(Number), loop(work)|Conts]).
+
+eval_read(Number, Conts) :-
+    read(Number),
+    continue(Conts).
+
+eval_work_(0, Conts) :-
+    writeln('I\'ve seen enough'),
+    continue([end|Conts]).
+
+eval_work_(N, Conts) :-
+    N > 0,
+    continue([writeln('That\'s a nice number.')|Conts]).
+
+eval_end(_).
+
+eval_writeln(X, Conts) :-
+    writeln(X),
+    continue(Conts).
+
+continue([]).
+continue([work_(Number)|Conts]) :-
+    eval_work_(Number, Conts).
+continue([loop(work)|Conts]) :-
+    eval_loop_work(Conts).
+continue([end|Conts]) :-
+    eval_end(Conts).
+continue([writeln(X)|Conts]) :-
+    eval_writeln(X, Conts).
